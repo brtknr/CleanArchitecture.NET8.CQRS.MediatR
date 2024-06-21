11e56472c068ffddd6fc87b2d0789953d06d63f1
Berat Kanar <berat.kanar@muratlikarton.com.tr>
2024-06-21 08:31:07 +0300

    .

---
+++ CleanArchitecture.Application/Errors/ValidationError.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanArchitecture.Application.Errors
+{
+    public class ValidationError
+    {
+        public string propertyName { get; private set; }
+        public string errorMessage { get; private set; }
+
+        public ValidationError(string propertyName, string errorMessage)
+        {
+            this.propertyName = propertyName;
+            this.errorMessage = errorMessage;
+        }
+    }
+}
